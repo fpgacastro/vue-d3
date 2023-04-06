3052ab86745809cb6b471932913f924463d9806e
castro <feihours@sina.com>
2023-04-06 23:04:33 +0800

    add the vue-router.

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>{{ text }}</div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  data() {
+    return {
+      // text: 'This is Home Page',
+    };
+  },
+});
+</script>
